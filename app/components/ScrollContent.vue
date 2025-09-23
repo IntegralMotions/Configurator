98ae1a84520ae060268f54df313468410d173af8
JosWigchert <joswigchert@gmail.com>
2025-09-23 23:13:14 +0200

    Update ScrollContent.vue, SerialOutput.vue, and 3 more files...

--- app/components/ScrollContent.vue
+++ app/components/ScrollContent.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex flex-col w-full">
+  <div class="flex flex-col w-full grow">
     <div v-if="$slots.header" class="flex-shrink-0">
       <slot name="header" />
     </div>
 
-    <div class="flex-1 min-h-0" :class="horizontal ? 'overflow-x-auto' : 'overflow-y-auto'">
+    <div class="flex-1 min-h-0 overflow-auto flex">
       <slot />
     </div>
 
